bc619371fa97cdafa93d2225486ffa0ba7327d5d
sudharshan <49453742+sudharshanacharya@users.noreply.github.com>
2020-08-01 21:23:49 +0530

    Add files via upload

--- the_lazy_code_2.0/the_lazy_code_2.0.ino
+++ the_lazy_code_2.0/the_lazy_code_2.0.ino
@@ -1,7 +1,4 @@
 #include <IRremote.h>
-#include <EEPROM.h>
-
-/////////////////////////////////////
 
 const int channel1 = 3;
 const int channel2 = 4;
@@ -12,7 +9,7 @@ const int channel4 = 6;
 (1)--here in the relay module high resembles off state & low resembles on  state
 */
 
-bool switch1 =  false;
+bool switch1 = false;
 bool switch2 = false;
 bool switch3 = false;
 bool switch4 = false;
@@ -22,15 +19,6 @@ const int ir_data = 8;
 IRrecv irrecv(ir_data);
 decode_results results;
 
-int addr[4];
-///////////////////////////////////
-
-void preStart(){
-   digitalWrite(channel1,HIGH);
-   digitalWrite(channel2,EEPROM.read(addr[1]));
-   digitalWrite(channel3,EEPROM.read(addr[2]));
-   digitalWrite(channel4,EEPROM.read(addr[3]));
-  }
 
 void setup(){
   Serial.begin(9600);
@@ -39,7 +27,6 @@ void setup(){
    pinMode(channel3,OUTPUT);
    pinMode(channel4,OUTPUT);
    irrecv.enableIRIn();
-   preStart();
 }
                                               
 
@@ -48,23 +35,16 @@ void loop(){
           //Serial.println(results.value, HEX);
       switch(results.value){
          case 0x207BEF0F:                                       //channel 1
-               if(EEPROM.read(addr[0]))
+               if(switch1)
                {
                   digitalWrite(channel1,HIGH);
                   Serial.println("channel1 is off state");
-                  EEPROM.write(addr[0],0 );
-                  Serial.println(EEPROM.read(addr[0]));
-                  
-                  
                   switch1 = false;
                 }
                 else
                 {
                   digitalWrite(channel1,LOW);
                   Serial.println("channel1 is on state");
-                  EEPROM.write(addr[0],1);
-                  Serial.println(EEPROM.read(addr[0]));
-                  
                   switch1 = true;
 
                  } 
@@ -75,18 +55,12 @@ void loop(){
                {
                   digitalWrite(channel2,HIGH);
                   Serial.println("channel2 is off state");
-                  EEPROM.write(addr[1],switch2);
-                  Serial.println(EEPROM.read(addr[1]));
-                  
                   switch2 = false;
                 }
                 else
                 {
                   digitalWrite(channel2,LOW);
                   Serial.println("channel2 is on state");
-                  EEPROM.write(addr[1],switch2);
-                  Serial.println(EEPROM.read(addr[1]));
-                  
                   switch2 = true;
                  }
          break;
@@ -96,9 +70,6 @@ void loop(){
               {
                 digitalWrite(channel3,HIGH);
                 Serial.println("channel3 is off state");
-                EEPROM.write(addr[2],switch3);
-                 Serial.println(EEPROM.read(addr[2]));
-
                 switch3 = false;
                 
                }
@@ -106,9 +77,6 @@ void loop(){
                  {
                   digitalWrite(channel3,LOW);
                   Serial.println("channel3 is on state");
-                  EEPROM.write(addr[2],switch3);
-                  Serial.println(EEPROM.read(addr[2]));
-                  
                   switch3 = true;
                  }
          break;
@@ -117,9 +85,6 @@ void loop(){
                 if(switch4){
                   digitalWrite(channel4,HIGH);;
                   Serial.println("channel4 is off state");
-                  EEPROM.write(addr[3],switch4);
-                  Serial.println(EEPROM.read(addr[3]));
-
                   switch4 =  false;
                   
                 }
@@ -127,9 +92,6 @@ void loop(){
                 {
                    digitalWrite(channel4,LOW);
                    Serial.println("channel4 is on state");
-                   EEPROM.write(addr[3],switch4); 
-                  Serial.println(EEPROM.read(addr[3]));
-                  
                    switch4 = true;
                    
                  }
